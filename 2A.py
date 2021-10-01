d292bf6ad243e48411a1704bb2f60a1654262113
PembangunanSIG <87271905+PembangunanSIG@users.noreply.github.com>
2021-10-01 20:09:24 +0700

    Kamera Sederhana
    Program sederhana untuk mengambil foto

--- 2A.py
+++ 2A.py
@@ -1,11 +1,13 @@
 import cv2
-cap = cv2.VideoCapture(0)
 
-def webcam():
-    while(True):
-        ret, frame = cap.read() 
-        cv2.imshow('WebCam',frame)
-        cv2.waitKey(1)
-        return frame
+def foto(sumber):
+    cv2.imwrite('hasil/nuur.png', sumber)
 
-webcam()
+cap = cv2.VideoCapture(1)
+while(True):
+    ret, frame = cap.read() 
+    cv2.imshow('WebCam',frame)
+    if cv2.waitKey(1) == ord('Q'):
+        break
+    elif cv2.waitKey(1) == ord('K'):
+        foto(frame)
